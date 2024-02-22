3263ae089e67ad093f076a38267b917b627dbf06
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-22 11:22:37 -0800

    making startup.r the main loop

--- R/startup.R
+++ R/startup.R
@@ -2,11 +2,39 @@
 
 # install.packages("httr2")
 # install.packages("devtools")
+# install.packages("openmeteo")
 
 library(devtools)
+library(openmeteo)
+library(httr2)
 
 devtools::install_github("mnr/sprinklR")
 
-create_waterByZone.R #create the matrix if necessary
+create_waterByZone() #create the matrix if necessary
 
-main_loop.R
+readRDS("waterByZone.RDS") # retrieve zone watering matrix
+
+# get the day of the year
+# January 1 = 0
+yearDay <- as.POSIXlt(Sys.Date())$yday
+
+update_waterbyzone(waterByZone,yearDay) # update with current forecasts
+
+saveRDS(waterByZone, "waterByZone.RDS") # save this update
+
+# Calculate needed irrigation ---------------------------------------------
+
+
+# Trigger irrigation ------------------------------------------------------
+
+irrigate(TRUE, 1) # turn on front yard
+irrigate(TRUE, 2) # turn on back yard
+Sys.sleep(100)
+irrigate(FALSE, 1) # turn off front yard
+Sys.sleep(100)
+irrigate(FALSE, 2) # turn off back yard
+
+# Send a heartbeat --------------------------------------------------------
+
+send_heartbeat()
+
