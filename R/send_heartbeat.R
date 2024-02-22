aae3c34b49a74bd0ac34db7081d87e660b1a5bc7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-22 11:22:16 -0800

    simplified by removing local information from parameters

--- R/send_heartbeat.R
+++ R/send_heartbeat.R
@@ -7,13 +7,23 @@
 #' @export
 #'
 #' @examples
-send_heartbeat <- function(iam_to_send) {
+send_heartbeat <- function() {
+  # get ip address
+  theIPaddress <- system("hostname -I", intern = TRUE) |>
+    strsplit(" ") |>
+    unlist() |>
+    (\(x) {x[1]})()
+
+  # get uptime
+  last_reboot <- system("uptime -s")
+
   heartbeat_request <- request("https://niemannross.com")
 
   http_request |>
     req_url_path_append("sprinklR") |>
     req_url_path_append("heartbeat.php") |>
-    req_url_query(iam = iam_to_send)
+    req_url_query(iam = theIPaddress) |>
+    req_url_query(uptime = last_reboot)
 
   http_response <- req_perform(http_request)
 }
