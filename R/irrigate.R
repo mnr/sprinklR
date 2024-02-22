d8e27f85ce7708d9015d722424071de1d7d35cb8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-22 11:21:46 -0800

    add zoneToPin

--- R/irrigate.R
+++ R/irrigate.R
@@ -10,5 +10,9 @@
 #'
 #' @examples
 irrigate <- function(state, zone) {
+  # zone #1 connected to GPIO17, board pin 11.
+  # zone #2 connected to GPIO27, board pin 13.
+  zoneToPin <- c(11,13)
 
+  rpi_set(pin_number = zoneToPin[zone], onOff = state) # turn both relays on
 }
