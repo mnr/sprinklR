07331507b589d49076557ace509d1dc02c1c443e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-27 15:50:37 -0800

    add documentation to create_waterByZone

--- R/create_waterByZone.R
+++ R/create_waterByZone.R
@@ -1,5 +1,15 @@
 # create the watering matrix and save to disk if necessary
 
+#' create_waterByZone
+#'
+#' Set up an initial matrix to track weather and watering
+#'
+#' creates waterByZone.RDS on local file system
+#'
+#' @return void
+#' @export
+#'
+#' @examples
 create_waterByZone <- function() {
   if (!file.exists("waterByZone.RDS")) {
     waterByZone <- matrix(
