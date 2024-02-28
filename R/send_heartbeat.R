04268f1398f62d15f6b72cafbf6e21e922e2b2c6
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-28 11:28:38 -0800

    fix req_body_json with data

--- R/send_heartbeat.R
+++ R/send_heartbeat.R
@@ -28,11 +28,12 @@ send_heartbeat <- function() {
   http_request <- request("https://niemannross.com") |>
     req_url_path_append("sprinklR") |>
     req_url_path_append("heartbeat.php") |>
-    req_body_json(iam = theIPaddress) |>
-    req_body_json(last_reboot = reboot_datetime) |>
-    req_body_json(wbz_rainfall = waterByZone["rainfall",], digits = 4) |>
-    req_body_json(wbz_zone1 = waterByZone["neededInFront",], digits = 4) |>
-    req_body_json(wbz_zone1 = waterByZone["neededInRear",], digits = 4)
+    req_body_json(list(iam = theIPaddress,
+                       last_reboot = reboot_datetime,
+                       wbz_rainfall = waterByZone["rainfall",],
+                       wbz_zone1 = waterByZone["neededInFront",],
+                       wbz_zone1 = waterByZone["neededInRear",]),
+                  digits = 4)
 
   http_response <- req_perform(http_request)
 }
