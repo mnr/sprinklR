f02c395452e307384a90cd9bbec978a1504d996a
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-29 06:29:27 -0800

    Adjusted for Portland, Oregon weather and watering schedule

--- R/create_waterByZone.R
+++ R/create_waterByZone.R
@@ -2,24 +2,37 @@
 
 #' create_waterByZone
 #'
-#' Set up an initial matrix to track weather and watering
+#' Sets up an initial matrix to track weather and watering
 #'
 #' creates waterByZone.RDS on local file system
+#' 1 inch (25.4 mm) of water per week
+#' Watering split over two sessions per week
+#' Last frost: March 15
+#' First frost: November 25
+#'
+#' @param forceWrite logical, default = TRUE. Overwrite the existing waterByZone?
 #'
 #' @return void
 #' @export
 #'
 #' @examples
-create_waterByZone <- function() {
-  if (!file.exists("waterByZone.RDS")) {
+create_waterByZone <- function(forceWrite = TRUE) {
+  if (forceWrite) {
+    # calculate last and first frost
+    last_frost <- as.POSIXlt("2024-03-15")$yday + 1 # March 15
+    first_frost <- as.POSIXlt("2024-11-25")$yday + 1 # November 25
+    irrigateVector <- c(rep(0, last_frost),
+                        rep(25.4, first_frost - last_frost),
+                        rep(0, 366 - first_frost))
+    rainfallVector <- rep(0, 366)
+
     waterByZone <- matrix(
-      data = NA,
+      data = c(rainfallVector, irrigateVector, irrigateVector),
+      byrow = TRUE,
       nrow = 3,
-      ncol = 365,
       dimnames = list(c(
         "rainfall", "neededInFront", "neededInRear"
-      ),
-      c())
+      ))
     )
 
     saveRDS(waterByZone, "waterByZone.RDS")
