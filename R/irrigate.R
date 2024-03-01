539ac7793cf4598dd5699d674c7549d13f4b20e1
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-29 16:32:22 -0800

    Changed from on/off to duration

--- R/irrigate.R
+++ R/irrigate.R
@@ -2,18 +2,24 @@
 #'
 #' Turns the irrigation system on and off
 #'
-#' @param state Logical. TRUE = on, FALSE = off
+#' This is a blocking function.
+#'   It will not return until the duration is complete
+#'
 #' @param zone numerical. 1 = front yard, 2 = back yard
+#' @param duration seconds of water for this zone
 #'
 #' @return nothing
 #' @export
 #'
 #' @examples
 #'
-irrigate <- function(state, zone) {
+irrigate <- function(zone, duration) {
   # zone #1 connected to GPIO17, board pin 11.
   # zone #2 connected to GPIO27, board pin 13.
   zoneToPin <- c(11,13)
 
-  rpi_set(pin_number = zoneToPin[zone], onOff = state) # turn both relays on
+  rpi_set(pin_number = zoneToPin[zone], onOff = 1)
+  Sys.sleep(duration)
+  rpi_set(pin_number = zoneToPin[zone], onOff = 0)
+
 }
