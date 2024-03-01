f8b2098b844088da82674c6bbe874cead50f3d1d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-29 23:37:28 -0800

    Trap out irrigation less than 30 seconds

--- R/irrigate.R
+++ R/irrigate.R
@@ -3,7 +3,8 @@
 #' Turns the irrigation system on and off
 #'
 #' This is a blocking function.
-#'   It will not return until the duration is complete
+#'   It will not return until the duration is complete.
+#'   duration must be greater than 30 seconds
 #'
 #' @param zone numerical. 1 = front yard, 2 = back yard
 #' @param duration seconds of water for this zone
@@ -16,10 +17,11 @@
 irrigate <- function(zone, duration) {
   # zone #1 connected to GPIO17, board pin 11.
   # zone #2 connected to GPIO27, board pin 13.
-  zoneToPin <- c(11,13)
+  zoneToPin <- c(11, 13)
 
-  rpi_set(pin_number = zoneToPin[zone], onOff = 1)
-  Sys.sleep(duration)
-  rpi_set(pin_number = zoneToPin[zone], onOff = 0)
-
+  if (duration > 30) {
+    rpi_set(pin_number = zoneToPin[zone], onOff = 1)
+    Sys.sleep(duration)
+    rpi_set(pin_number = zoneToPin[zone], onOff = 0)
+  }
 }
