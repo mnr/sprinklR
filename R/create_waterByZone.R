7f8b97eb607998c9385e78b935f74f25464bd79e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-03-01 12:14:11 -0800

    clarify inches to mm

--- R/create_waterByZone.R
+++ R/create_waterByZone.R
@@ -22,7 +22,7 @@ create_waterByZone <- function(forceWrite = TRUE) {
     last_frost <- as.POSIXlt("2024-03-15")$yday + 1 # March 15
     first_frost <- as.POSIXlt("2024-11-25")$yday + 1 # November 25
     # how much to water in each zone
-    mmWaterPerWeek <-  25.4
+    mmWaterPerWeek <-  25.4 # one inch per week = 25.4 mm
     irrigateVector <- c(rep(0, last_frost),
                         rep_len(c(0,0,mmWaterPerWeek/2,0,mmWaterPerWeek/2,0,0), first_frost - last_frost),
                         rep(0, 366 - first_frost))
