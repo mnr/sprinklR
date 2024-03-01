3124083e2f4b6c9f6bae76915969d7fb9926c8d4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-02-29 17:18:29 -0800

    renamed as x.php. Chart now works

--- inst/niemannross_sprinklR/index.php
+++ inst/niemannross_sprinklR/index.php
@@ -23,23 +23,29 @@
 <script>
   const ctx = document.getElementById('myChart');
 
+//'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+
   new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        labels: [<?php echo implode(", ", range(1,366)) ?>],
       datasets: [{
         label: 'Rainfall',
-        data: [<?php $json_data["wbz_rainfall"] ?>],
-        borderWidth: 1
+        data: [<?php echo implode(", ",$json_data["wbz_rainfall"]) ?>],
+        tension: 0.1
       }, {
-          label: 'Zone One',
-          data: [<?php $json_data["wbz_zone1"] ?>],
-          borderWidth: 1
-      },{
-          label: 'Zone Two',
-          data: [<?php $json_data["wbz_zone2"] ?>],
-          borderWidth: 1
-      }]
+          label: 'Zone One needed',
+          data: [<?php echo implode(", ",$json_data["wbz_NeededZone1"]) ?>],
+        tension: 0.1      },{
+          label: 'Zone Two needed',
+          data: [<?php echo implode(", ",$json_data["wbz_NeededZone2"]) ?>],
+        tension: 0.1      },{
+          label: 'Zone one watered',
+          data: [<?php echo implode(", ",$json_data["wbz_WateredZone1"]) ?>],
+        tension: 0.1      },{
+          label: 'Zone Two watered',
+          data: [<?php echo implode(", ",$json_data["wbz_WateredZone2"]) ?>],
+        tension: 0.1      }]
     },
     options: {
       scales: {
@@ -56,8 +62,5 @@
     <p><?php echo "Last heartbeat: " . date("Y-m-d H:i:s", $lastHeartBeat)?></p>
     <p><?php echo "IP Address: " . $json_data["iam"] ?></p>
     <p><?php echo "Last Reboot: " . $json_data["last_reboot"] ?></p>
-    <p><?php echo "Rainfall: " . $json_data["wbz_rainfall"][10] ?></p>
-    <p><?php echo "Zone 1: " . $json_data["wbz_zone1"] ?></p>
-    <p><?php echo "Zone 2: " . $json_data["wbz_zone2"] ?></p>
 </body>
 </html>
