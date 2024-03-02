3b4a11675ee9bca5f2339d28625c820f0c51f59d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-03-02 13:48:36 -0800

    add save seconds watered to waterByZone matrix

--- inst/startup.R
+++ inst/startup.R
@@ -23,14 +23,20 @@ waterByZone <- update_waterbyzone(waterByZone, yearDay) # update with current fo
 # Calculate needed irrigation ---------------------------------------------
 waterByZone <- howMuchToWater(waterByZone,yearDay)
 
-saveRDS(waterByZone, "waterByZone.RDS") # save this update
-
 # Trigger irrigation ------------------------------------------------------
 
-irrigate(1, conv_mm_to_duration(waterByZone["wateredInFront",yearDay])) # turn on front yard
-irrigate(2, conv_mm_to_duration(waterByZone["wateredInFront",yearDay])) # turn on back yard
-# irrigate(1,waterByZone["wateredInFront",yearDay]/mmWaterPerSecond) # turn on front yard
-# irrigate(2,waterByZone["wateredInFront",yearDay]/mmWaterPerSecond) # turn on back yard
+# water in front
+waterFrontSeconds <- conv_mm_to_duration(waterByZone["wateredInFront",yearDay])
+irrigate(1, waterFrontSeconds) # turn on front yard
+waterByZone["secondsWateredInFront", yearDay] <- waterFrontSeconds
+
+# water in rear
+waterRearSeconds <- conv_mm_to_duration(waterByZone["wateredInRear",yearDay])
+irrigate(2, waterRearSeconds) # turn on back yard
+waterByZone["secondsWateredInRear", yearDay] <- waterRearSeconds
+
+# save all of these calculations
+saveRDS(waterByZone, "waterByZone.RDS")
 
 # Send a heartbeat --------------------------------------------------------
 
