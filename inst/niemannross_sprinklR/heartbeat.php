c8b3331f0818ed4f60a114fac11b1dbeb7ed4589
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-03-05 15:06:27 -0800

    add modification date time

--- inst/niemannross_sprinklR/heartbeat.php
+++ inst/niemannross_sprinklR/heartbeat.php
@@ -3,6 +3,11 @@
   // Takes raw data from the request
   $json = file_get_contents('php://input');
 
+  // add dateTime to json
+  $json_tmp = json_decode($json) ;
+  $json_tmp['modified'] = date("Y-m-d H:i:s") ;
+  $json = json_encode($json_tmp) ;
+
   // save to a local file
   $fp = fopen("heartbeat_data.json", "w") ;
   fwrite($fp, $json) ;
