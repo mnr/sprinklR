b642ae6e723acd1b01df05c87d7a896d992ed097
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-03-20 20:46:35 -0700

    add evapotranspiration to calculation

--- R/howMuchToWater.R
+++ R/howMuchToWater.R
@@ -3,7 +3,8 @@
 #' How Much To Water each day
 #'
 #' compares rainfall from yesterday, today, and tomorrow
-#'    against today's needed rainfall
+#'    minus evapotranspiration
+#'    against today's needed amount of water
 #'
 #' @param waterByZone matrix showing rainfall, needed and watered
 #' @param yearDay day of the year. January 1 = 1
@@ -17,11 +18,15 @@ howMuchToWater <- function(waterByZone, yearDay) {
 
   recentRainfall <-
     sum(waterByZone["rainfall", (yearDay - 1):(yearDay + 1)])
+  recentEVOTRP <- sum(waterByZone["evapotranspiration", (yearDay - 1):(yearDay + 1)])
+
   # calculate needed water for front zone
-  neededRain <- waterByZone["neededInFront", yearDay] - recentRainfall
+  neededRain <- waterByZone["neededInFront", yearDay] - recentRainfall + recentEVOTRP
   waterByZone["wateredInFront", yearDay] <- ifelse(neededRain <= 0, 0, neededRain)
+
   # calculate needed water for rear zone
-  neededRain <- waterByZone["neededInRear", yearDay] - recentRainfall
+  neededRain <- waterByZone["neededInRear", yearDay] - recentRainfall + recentEVOTRP
   waterByZone["wateredInRear", yearDay] <- ifelse(neededRain <= 0, 0, neededRain)
+
   return(waterByZone)
 }
