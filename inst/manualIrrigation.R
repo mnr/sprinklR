e672b05ea6767219535cc93daf856b27bd28808c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-03-25 17:24:24 -0700

    manually turns on irrigation

---
+++ inst/manualIrrigation.R
@@ -0,0 +1,21 @@
+# Handle a request for manual irrigation
+
+# cron runs every 10 seconds
+# This program watches for a two way switch
+# position one turns on zone 1
+# position two turns on zone 2
+# position central returns to automatic
+
+library(sprinklR)
+library(rpigpior)
+
+zoneToPin <- c(15, 19)
+
+while(rpi_get(zoneToPin[1])) {
+    irrigate(1,1)
+}
+
+while(rpi_get(zoneToPin[2])) {
+  irrigate(2,1)
+}
+
