123a08a59e307f3e85a33b25f49e87c68945d907
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-04-10 04:27:37 -0700

    if neededRain = 0, then wateredInFront = 0

--- R/howMuchToWater.R
+++ R/howMuchToWater.R
@@ -17,16 +17,26 @@ howMuchToWater <- function(waterByZone, yearDay) {
   # get the sum rainfall for day before yesterday, yesterday, today, tomorrow, and day after tomorrow
 
   recentRainfall <-
-    sum(waterByZone["rainfall", (yearDay - 2):(yearDay + 2)])
-  recentEVOTRP <- sum(waterByZone["evapotranspiration", (yearDay - 2):(yearDay + 2)])
+    sum(waterByZone["rainfall", (yearDay - 1):(yearDay + 1)])
+  recentEVOTRP <- sum(waterByZone["evapotranspiration", (yearDay - 1):(yearDay + 1)])
 
   # calculate needed water for front zone
-  neededRain <- waterByZone["neededInFront", yearDay] - recentRainfall + recentEVOTRP
-  waterByZone["wateredInFront", yearDay] <- ifelse(neededRain <= 0, 0, neededRain)
+  if (waterByZone["neededInFront", yearDay] > 0) {
+    neededRain <- waterByZone["neededInFront", yearDay] - recentRainfall + recentEVOTRP
+    waterByZone["wateredInFront", yearDay] <- ifelse(neededRain <= 0, 0, neededRain)
+  } else {
+    waterByZone["wateredInFront", yearDay] <- 0
+  }
 
   # calculate needed water for rear zone
   neededRain <- waterByZone["neededInRear", yearDay] - recentRainfall + recentEVOTRP
   waterByZone["wateredInRear", yearDay] <- ifelse(neededRain <= 0, 0, neededRain)
+  if (waterByZone["neededInRear", yearDay] > 0) {
+    neededRain <- waterByZone["neededInRear", yearDay] - recentRainfall + recentEVOTRP
+    waterByZone["wateredInRear", yearDay] <- ifelse(neededRain <= 0, 0, neededRain)
+  } else {
+    waterByZone["wateredInRear", yearDay] <- 0
+  }
 
   return(waterByZone)
 }
