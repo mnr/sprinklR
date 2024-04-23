01d35acde0af252f5dda591a3d885f8ecb4c13d9
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-04-22 23:21:00 -0700

    add check for valid data

--- inst/niemannross_sprinklR/heartbeat.php
+++ inst/niemannross_sprinklR/heartbeat.php
@@ -3,15 +3,19 @@
   // Takes raw data from the request
   $json = file_get_contents('php://input');
 
-  // add dateTime to json
-  $json_tmp = json_decode($json, true) ;
-  $json_tmp['modified'] = date("Y-m-d H:i:s") ;
-  $json = json_encode($json_tmp) ;
+  // check for valid json input
+  if (isset($json['wbz_rainfall'])) {
 
-  // save to a local file
-  $fp = fopen("heartbeat_data.json", "w") ;
-  fwrite($fp, $json) ;
-  fclose($fp);
+    // add dateTime to json
+    $json_tmp = json_decode($json, true) ;
+    $json_tmp['modified'] = date("Y-m-d H:i:s") ;
+    $json = json_encode($json_tmp) ;
+
+    // save to a local file
+    $fp = fopen("heartbeat_data.json", "w") ;
+    fwrite($fp, $json) ;
+    fclose($fp);
+  }
 
   exit();
 ?>
