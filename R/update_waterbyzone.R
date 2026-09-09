5a97061b92c36e39256df0ad572a1d5c65decb3f
Mark Niemann-Ross <mark.niemannross@gmail.com>
2026-09-09 00:40:11 -0700

    repair "Error in req_perform(meteo_request) : HTTP 400 Bad Request."

--- R/update_waterbyzone.R
+++ R/update_waterbyzone.R
@@ -26,7 +26,9 @@ update_waterbyzone <- function(waterByZone, yearDay) {
     req_url_query(longitude = "-122.6762") |>
     # req_url_query(current = "precipitation") |>
     # req_url_query(daily = "precipitation_sum,precipitation_probability_max,et0_fao_evapotranspiration") |>
-    req_url_query(daily="precipitation_sum,et0_fao_evapotranspiration&timezone=America%2FLos_Angeles&forecast_days=3") |>
+    req_url_query(daily="precipitation_sum,et0_fao_evapotranspiration") |>
+    req_url_query(timezone="America%2FLos_Angeles") |>
+    req_url_query(forecast_days="3") |>
     req_retry(retry_on_failure = TRUE, max_tries = 4)
 
     try(
